2facf92dbb61cbdf3b10c9e8fabf7f05fe95caa5
ShubhamSharma-Exp <ssharma9106@gmail.com>
2021-04-01 14:19:52 +0530

    Connecting application with the database

--- BloggingApplication/backend/app.js
+++ BloggingApplication/backend/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://Shubham:Ss6wyJ3iRFLwDZ3@cluster0.xtqkn.mongodb.net/UserPosts?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(() => {
+    console.log("Cannot connect to the database");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,29 +34,33 @@ app.use((req, res, next) => {
 });
 
 app.post("/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: "Post added Successfully",
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then((createdPost) => {
+    res.status(201).json({
+      message: "Post added Successfully",
+      postId: createdPost._id,
+    });
   });
 });
 
 app.get("/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "qkdfnkqwd12",
-      title: "My First Post",
-      content: "This is coming from the server",
-    },
-    {
-      id: "qkdfasdad12",
-      title: "My Second Post",
-      content: "This is coming from the server!",
-    },
-  ];
-  return res.status(200).json({
-    message: "Posts fetched successfully",
-    posts: posts,
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched successfully",
+      posts: documents,
+    });
+  });
+});
+
+app.delete("/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+    res.status(200).json({
+      message: "Post Deleted",
+    });
   });
 });
 
